5ff36eaf934d152aec4503a5d7c3e8ff50df8f84
Alek30k <alemank23k@gmail.com>
2025-12-14 18:06:09 -0300

    last commit

--- .next/dev/static/chunks/app_layout_tsx_6ce19d2f._.js
+++ .next/dev/static/chunks/app_layout_tsx_6ce19d2f._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__51d697a8._.css",
-  "static/chunks/node_modules__pnpm_1e1d64d7._.js"
+  "static/chunks/node_modules__pnpm_b9f422dc._.js"
 ],
     source: "dynamic"
 });
